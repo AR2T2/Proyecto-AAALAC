28ee2f212a6a6b5bb5a1233a5832984059a30811
xoyoc <xoyoc_l2@hotmail.com>
2016-06-14 14:25:08 -0500

    Modificacion de migracion

--- database/migrations/2016_03_29_205358_crear_registro.php
+++ database/migrations/2016_03_29_205358_crear_registro.php
@@ -12,7 +12,30 @@ class CrearRegistro extends Migration
      */
     public function up()
     {
-        Schema::create('registro', function (Blueprint $table) {
+        //Primera tabla
+        Schema::create('agentes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Nombre_agente_aduanal');
+            $table->string('Patente',4);
+            $table->string('Correo_aa');
+            $table->timestamps();
+        });
+        // segunda tabla
+        Schema::create('agencias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Nombre_agencia', 50);
+            $table->string('Gerente_agencia', 50);
+            $table->string('Jefe_operaciones', 50);
+            $table->string('Direccion', 50);
+            $table->integer('Telefono');
+            $table->string('Correo_gte', 50);
+            $table->string('Correo_operaciones', 50);
+            $table->string('Correo_administracion', 50);
+            $table->string('Jefe_administracion', 50);
+            $table->timestamps();
+        });
+        // tercera tabla
+        Schema::create('registros', function (Blueprint $table) {
             $table->increments('id');
             $table->string('RFC',10);
             $table->string('CURP',10);
@@ -21,6 +44,27 @@ class CrearRegistro extends Migration
             $table->string('Pago',2);
             $table->timestamps();
         });
+        // cuarta tabla
+        Schema::create('status', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('campoStatus',['Alta','Baja','Proceso','Suspendido']);
+            $table->string('observacion');
+            $table->timestamps();
+        });
+        // quinta tabla
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre_proyecto');
+            $table->timestamps();
+        });
+        // sexta tabla
+        Schema::create('usuariosaaalac', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('usuario');
+            $table->string('contraseña');
+            $table->timestamps();
+        });
+
     }
 
     /**
@@ -30,6 +74,11 @@ class CrearRegistro extends Migration
      */
     public function down()
     {
-        Schema::drop('registro');
+        Schema::drop('agentes');
+        Schema::drop('agencias');
+        Schema::drop('registros');
+        Schema::drop('status');
+        Schema::drop('proyectos');
+        Schema::drop('usuariosaaalac');
     }
 }
