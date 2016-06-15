58b093edd639040c4bc2352f65314ea959439816
xoyoc <xoyoc_l2@hotmail.com>
2016-06-15 09:44:05 -0500

    Modificacion de la migracion cambio de proyectos por servicios relacion

--- database/migrations/2016_03_29_205358_crear_registro.php
+++ database/migrations/2016_03_29_205358_crear_registro.php
@@ -13,25 +13,16 @@ class CrearRegistro extends Migration
     public function up()
     {
         //Primera tabla
-        Schema::create('agentes', function (Blueprint $table) {
+        Schema::create('usuariosaaalac', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('Nombre_agente_aduanal');
-            $table->string('Patente',4);
-            $table->string('Correo_aa');
+            $table->string('usuario');
+            $table->string('contraseña');
             $table->timestamps();
         });
         // segunda tabla
-        Schema::create('agencias', function (Blueprint $table) {
+        Schema::create('servicios', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('Nombre_agencia', 50);
-            $table->string('Gerente_agencia', 50);
-            $table->string('Jefe_operaciones', 50);
-            $table->string('Direccion', 50);
-            $table->integer('Telefono');
-            $table->string('Correo_gte', 50);
-            $table->string('Correo_operaciones', 50);
-            $table->string('Correo_administracion', 50);
-            $table->string('Jefe_administracion', 50);
+            $table->string('nombre_servicio');
             $table->timestamps();
         });
         // tercera tabla
@@ -52,19 +43,37 @@ class CrearRegistro extends Migration
             $table->timestamps();
         });
         // quinta tabla
-        Schema::create('proyectos', function (Blueprint $table) {
+        Schema::create('agentes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nombre_proyecto');
+            $table->string('Nombre_agente_aduanal');
+            $table->string('Patente',4);
+            $table->string('Correo_aa');
+            $table->integer('registros_id')->unsigned(); // llave foranea de status
+            $table->foreign('registros_id')->references(id)->on('registros');            
+            $table->integer('status_id')->unsigned(); // llave foranea de status
+            $table->foreign('status_id')->references(id)->on('status');
             $table->timestamps();
         });
         // sexta tabla
-        Schema::create('usuariosaaalac', function (Blueprint $table) {
+        Schema::create('agencias', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('usuario');
-            $table->string('contraseña');
+            $table->string('Nombre_agencia', 50);
+            $table->string('Gerente_agencia', 50);
+            $table->string('Jefe_operaciones', 50);
+            $table->string('Direccion', 50);
+            $table->integer('Telefono');
+            $table->string('Correo_gte', 50);
+            $table->string('Correo_operaciones', 50);
+            $table->string('Correo_administracion', 50);
+            $table->string('Jefe_administracion', 50);
+            $table->integer('agentes_id')->unsigned(); // llave foranea de agentes aduanales
+            $table->foreign('agentes_id')->references(id)->on('agentes');
+            $table->integer('servicios_id')->unsigned(); // llave foranea de servicios
+            $table->foreign('servicios_id')->references(id)->on('servicios');
+            $table->integer('usuariosaaalac_id')->unsigned(); // llave foranea de usuarios
+            $table->foreign('usuariosaaalac_id')->references(id)->on('usuariosaaalac');
             $table->timestamps();
         });
-
     }
 
     /**
@@ -74,11 +83,11 @@ class CrearRegistro extends Migration
      */
     public function down()
     {
-        Schema::drop('agentes');
-        Schema::drop('agencias');
+        Schema::drop('usuariosaaalac');
+        Schema::drop('servicios');
         Schema::drop('registros');
         Schema::drop('status');
-        Schema::drop('proyectos');
-        Schema::drop('usuariosaaalac');
+        Schema::drop('agentes');
+        Schema::drop('agencias');
     }
 }
